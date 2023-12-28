1c63539d43ac9f94a1cedd9540e938fc6b477ad3
prixhd <110771249+prixhd@users.noreply.github.com>
2023-12-28 23:27:13 +0300

    1

--- src/main/java/com/example/studentapplication/StudentService.java
+++ src/main/java/com/example/studentapplication/StudentService.java
@@ -1,8 +1,5 @@
 package com.example.studentapplication;
 
-// src/main/java/com/вашакомпания/вашеприложение/service/StudentService.java
-import jakarta.servlet.http.HttpServletRequest;
-
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 import java.util.List;
